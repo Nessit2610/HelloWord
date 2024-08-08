88a8e2f5f28a3abcaae2f15626e4637bfa92e1ea
TienLe26dev <letien26102003@gmail.com>
2024-08-08 15:01:39 +0700

    TienLe

--- src/main/java/com/example/demo/Rest/LoginController.java
+++ src/main/java/com/example/demo/Rest/LoginController.java
@@ -1,22 +1,36 @@
 package com.example.demo.Rest;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.Entity.User;
+import com.example.demo.Service.UserService;
 
 
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/login")
 	public String showLoginPage(){
         return "login";
     }
 	
-   
+	@GetMapping("/authenticate/{username}")
+	public String getUser(@PathVariable("username") String username) {
+		User user = userService.findByUsername(username);
+	   if(user == null) {
+		   return "Not found !";
+	   }
+		return user.getPassword();
+	}
+	
 	
   
 }
